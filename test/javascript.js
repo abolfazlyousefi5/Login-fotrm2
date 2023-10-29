b908179705fc1e838baf561b8bcd995377acaf61
abolfazl <afgboy2922@gmail.com>
2023-10-29 14:32:06 +0330

    aded login-form2

---
+++ test/javascript.js
@@ -0,0 +1,62 @@
+// //array is array......
+// var x = [1, 2, 3, 4, 5];
+// var y = ["abolfazl", "alireza", "omid", "abbas"];
+// var z;
+
+// //Method......
+// //toString()..(تبدیل ابجکت به استرینگ);
+
+// z = x.toString(x);
+
+// //Join()...(فاصله بین استرینگ ها);
+
+// z = x.join(" * ");
+// z = y.join(" * ");
+
+// //Add , Remove...(اضافه و حذف);
+// z = x.pop(); //حذف از اخر
+// z = x.push(6); //اضافه از اخر
+// z = x.shift(); //حذف از اول
+// z = x.unshift(0.9); //اضافه از اول
+
+// //Change... تغییر ایتم یک ارایه
+// x[1] = 12;
+
+// //splace().... حذف و اضافه کردن ایتم
+// x.splice(0 ,0 ,5);
+// x.splice(2, 0, 22);
+// x.splice(6, 1);
+// y.splice(3 ,0 ,"hashem");
+// //concat()...[ترکیب کردن متغیر ها با هم]
+// z = y.concat("hafiz"); //اخر اضافه میشه
+// z = ["hafiz"] .concat(y);  //اول اضافه مبشه
+
+// //Log.......
+// console.log(x);
+// console.log(typeof x);
+// console.log(Array.isArray(x));
+
+// console.log("-------");
+
+// console.log(z);
+// console.log(typeof z);
+// console.log(Array.isArray(z));
+
+//------------------------------------------------------
+// let x = [5, 8, 9, 6, 2];
+// let y = ["abol", "nrgs", "yas", "nazi"];
+
+// //sort()...[مرتب کردن از اول به اخر]
+// x.sort();
+// y.sort();
+
+// //revers()..[مرتب کردن از اخر به اول]
+// x.reverse();
+// y.reverse();
+
+// console.log(x);
+// console.log(y);
+
+//--------------------------------------------------
+
+
